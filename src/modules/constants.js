c175d36a7f7f92dc251b56f34b781ebdda833fc7
Kibo <tomasjurman@gmail.com>
2021-02-26 14:33:18 +0100

    0.1.0

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -1,17 +1,19 @@
 /**
- * The App main wrapper element ID
+ * The App main wrapper element ID.
  * 
  * @constant
  * @type {string}
  */
-export const ID_APP = "cz-kibo-meeting-chime-app";
+export const ID_APP = "cz-kibo-meeting-chime-client";
 			
 /**
  * Server credentials URL
  * 
- * From these URL the client ask for a Chime Meeting and Chime Attendee
+ * From these URL the client ask for a Chime Meeting and Chime Attendee.
+ * 
  * @constant
  * @type {string}
+ * 
  * @see server.js
  */
 export const CRENDTIALS_TO_MEETING_URL = "http://localhost:3000/meeting/";
