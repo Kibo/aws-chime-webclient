47d94236c8d896e52d1ba27ec9e83789c4d46fa8
Tom <tomasjurman@gmail.com>
2021-04-11 18:04:22 +0200

    v0.6.12

---
+++ src/modules/pdf/PDFViewer.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+
+<script>
+import Utils from "../tools/Utils.js"
+import { PDFJS } from 'pdfjs-dist'
+
+export default {
+  components: {},
+	emits: [],
+	props: [],
+	data() {
+			return {
+				utils:Utils,
+			  logger:this.$store.state.logger
+        pathToPdfFile:'/assets/testa.pdf'
+			}
+	},
+	mounted() {
+      PDFJS.getDocument( pathToPdfFile )
+  },
+	beforeUnmount(){
+
+  },
+	methods:{
+
+  }
+}
+</script>
